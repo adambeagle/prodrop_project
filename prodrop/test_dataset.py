ad22823ffb5132374fddccef2938417104639a2a
Adam Beagle <abeagle88@gmail.com>
2014-06-11 19:33:01 -0400

    Update/Bugfix test_dataset
    The test now verifies that all lines immediately following an empty line
    start with the "(TOP " tag

--- prodrop/test_dataset.py
+++ prodrop/test_dataset.py
@@ -4,11 +4,14 @@ Author: Adam Beagle
 
 PURPOSE:
     To computationally confirm assumptions being made about any treebank
-    data being used. Anything that can be deciseively ruled out as a
+    data being used. Anything that can be decisively ruled out as a
     potential pitfall in this file will save error-checking time elsewhere.
 
     The tests should be re-run and verified to pass whenever new treebank
     data is added to the project.
+    
+TODO:
+    * Test that a tag NEVER contains whitespace.
 """
 import unittest
 from util import get_files_by_ext
@@ -18,26 +21,32 @@ INPUT_DIR = '../treebank_data/00' # TODO allow multi-directory walk from root
 class TestTreeStart(unittest.TestCase):
     """ """
     parse_files = get_files_by_ext(INPUT_DIR, '.parse', prepend_dir=True)
+    
+    def test_parse_files(self):
+        """Verify .parse files discovered."""
+        self.assertTrue(self.parse_files)
 
     def test_top_token_uniqueness(self):
         """
         For each file, test that the TOP token is used ONLY:
-          1. At the immediate start of a line (following a left paren)
-          2. At the immediate start of a tree (i.e. after a blank line)
+        1. At the immediate start of a line (following a left paren)
+        2. At the immediate start of a tree (i.e. after a blank line)
         """
-        prev_blank = False
+        prev_blank = True # True so first line of file treated as new tree
 
         for path in self.parse_files:
             with open(path, encoding='utf8') as f: 
                 for i, line in enumerate(f):
-                    if 'TOP' in line:
+                    if prev_blank:
+                        self.assertTrue(line.startswith('(TOP '))
                         self.assertEqual(line.count('TOP'), 1)
-                        self.assertEqual(line[:4], '(TOP')
-                        self.assertTrue(prev_blank or i == 0)
                         prev_blank = False
+
                     elif line == '\n':
                         prev_blank = True
+
                     else:
+                        self.assertNotIn('(TOP', line)
                         prev_blank = False
 
 ###############################################################################
