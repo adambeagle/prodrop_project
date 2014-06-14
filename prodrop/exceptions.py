4deeccd6d8983026acfd47e08a8c187a7882bb27
Adam Beagle <abeagle88@gmail.com>
2014-06-14 11:02:04 -0400

    Update exceptions: Hierarchy for ParseTreeError

--- prodrop/exceptions.py
+++ prodrop/exceptions.py
@@ -1,11 +1,30 @@
+"""
+exceptions.py
+Author: Adam Beagle
+
+PURPOSE:
+  Contains all custom exception classes for the prodrop package.
+"""
+
+###############################################################################
+# ParseTreeError and iheritors
+class ParseTreeError(Exception):
+    """Base class for all parse tree related errors."""
+    pass
+
+class SearchFlagError(ParseTreeError):
+    pass
+
+class TreeConstructionError(ParseTreeError):
+    pass
+
+###############################################################################
+# Other
 class MissingParseFilesError(Exception):
     pass
 
 class NoTreesFoundError(Exception):
     pass
 
-class SearchFlagError(Exception):
-    pass
 
-class TreeConstructionError(Exception):
-    pass
+
