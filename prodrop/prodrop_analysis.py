074c781db84859ec34557328ce9dbf3bbb8a0239
Adam Beagle <abeagle88@gmail.com>
2014-06-15 12:40:27 -0400

    Update prodrop_analysis; verb association
    Now extracts associated verbs for all pro-drops, counts uses of each
    verb, and writes report.

--- prodrop/prodrop_analysis.py
+++ prodrop/prodrop_analysis.py
@@ -8,56 +8,79 @@ PURPOSE:
 """
 from constants import TREEBANK_DATA_PATH
 from parsetree import ParseTree
-from util import itertrees, Timer
+from util import itertrees, itertrees_dir, Timer
 
-INPUT_PATH = '../treebank_data/testdata/simple_trees.txt'
+INPUT_PATH = TREEBANK_DATA_PATH #'../treebank_data/testdata/simple_trees.txt'
+OUTPUT_PATH = 'report.txt'
 
-def get_prodrops(tree):
+def iterprodrops(tree):
     """
-    Return list of pro-drop nodes in a single ParseTree.
-    
-    In treebank notation, these are (-NONE- *) nodes whose parent is a variant
+    Yield pro-drop nodes, i.e. (-NONE- *) nodes whose parent is a variant
     of NP-SBJ.
     """
-    prodrops = []
+    return (node for node in tree.search(tag='-NONE-', word='*',
+        parent_tag='NP-SBJ', parent_flag=tree.STARTSWITH)
+    )
 
-    # Find nodes 
-    for node in tree.search(tag='-NONE-', word='*', parent='NP-SBJ',
-        parent_flag=tree.STARTSWITH
-    ):
-        for sibling in tree.get_siblings(node.parent):
-            if sibling.tag.startswith('IV'):
-                prodrops.append(node)
-
-    return prodrops
-
-def report_prodrops(tree):
+def prodrop_verb_association(treesfunc):
     """
     """
-    count = 0
+    prodrop_count = 0
+    prodrop_with_verb_count = 0
+    tree_count = 0
+    verbs = {}
+
+    print('Starting pro-drop search... ', end='')
+    for tree in treesfunc(INPUT_PATH):
+        tree_count += 1
+        for pdnode in iterprodrops(tree):
+            prodrop_count += 1
+            for sibling in tree.get_siblings(pdnode.parent):
+                if sibling.tag.startswith('IV'):
+                    prodrop_with_verb_count += 1
+                    _update_verbs(verbs, sibling.word)
+
+    print('Complete.')
+    _write_report(verbs, tree_count, prodrop_count, prodrop_with_verb_count)
     
-    for node in tree.search(tag='-NONE-', word='*', parent='NP-SBJ',
-        parent_flag=tree.STARTSWITH
-    ):
-        for sibling in tree.get_siblings(node.parent):
-            if sibling.tag.startswith('IV'):
-                count += 1
-                print('Pro-drop found!')
-                print('Verb tag: {0}'.format(sibling.tag))
-                print('Verb: {0}\n'.format(sibling.word))
+def _write_report(verbs, tree_count, prodrop_count, prodrop_with_verb_count):
+    """ """
+    outfile = open(OUTPUT_PATH, 'w', encoding='utf8')
+    
+    def print_and_write(s):
+        print(s)
+        outfile.write(s)
+        outfile.write('\n')
 
-    return count
+    print('')
+    print_and_write('Trees searched: {0}'.format(tree_count))
+    print_and_write('Pro-drops found: {0}'.format(prodrop_count))
+    print_and_write('Pro-drops with associated verb found: {0}'.format(
+        prodrop_with_verb_count
+    ))
+    print_and_write('Distinct verbs found: {0}'.format(len(verbs)))
+    
+    outfile.write('\nVERB OCCURENCES\n===============\n')
+    for v in sorted(verbs, key=lambda x: verbs[x], reverse=True):
+        outfile.write('{0:>2}'.format(verbs[v]))
+        outfile.write('  :  {0:<2}\n'.format(v))
+
+    
+    outfile.close()
+
+    print('\nReport written to {0}'.format(OUTPUT_PATH))
+
+def _update_verbs(verbs, newverb):
+    if newverb in verbs:
+        verbs[newverb] += 1
+    else:
+        verbs[newverb] = 1
 
 ###############################################################################
 if __name__ == '__main__':
-    with Timer() as timer:
-        count = 0
-        prodrop_count = 0
-        for tree in itertrees(INPUT_PATH, cache_end_nodes=1):
-            prodrop_count += report_prodrops(tree)
-            count +=1
+    try:
+        with Timer() as timer:
+            prodrop_verb_association(itertrees_dir)
+    finally:
+        print('Time: {0:.3f}s'.format(timer.total_time))
 
-        print('===================================')
-        print('Total trees scanned: {0}'.format(count))
-        print('Pro-drops found: {0}'.format(prodrop_count))
-        print('Time: {0:.3f}s'.format(timer.elapsed_time))
