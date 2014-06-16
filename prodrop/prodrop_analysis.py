c54ad97e0f1b88a51e23ea64343cf8580e16b4a6
Adam Beagle <abeagle88@gmail.com>
2014-06-16 14:43:11 -0400

    Update prodrop_verb_association. Large change
    After lots of time examining reports of various forms of the
    prodrop_verb_association method it's in a good state where 95% of
    pro-drops now have an associated verb found and the report is printing a
    lot of useful data.
    
    Next step is to refactor the verb association method, as it's too long
    and very ugly.

--- prodrop/prodrop_analysis.py
+++ prodrop/prodrop_analysis.py
@@ -16,19 +16,15 @@ INPUT_PATH = TREEBANK_DATA_PATH #'../treebank_data/testdata/simple_trees.txt'
 OUTPUT_PATH = '../reports/'
 PRODROP_WORD_PATTERN = '^\*(?:-\d+)?$'
 
-def get_previous_sibling(node):
+def get_previous_siblings(node):
     """
-    Return the prior sibling of a tree as found in a depth-first traversal.
+    Yield the prior siblings of a tree as found in a depth-first traversal.
     """
-    previous_sibling = None
-    
     for child in node.parent.children:
         if child == node:
-            return previous_sibling
-        
-        previous_sibling = child
+            return
 
-    return None
+        yield child
 
 def iterprodrops(tree):
     """
@@ -49,45 +45,54 @@ def prodrop_verb_association(treesfunc):
     prodrop_count = 0
     prodrop_with_verb_count = 0
     tree_count = 0
+    tree_with_prodrop_count = 0 # At least 1 pro-drop
     verbs = {}
     ignored_tags = {}
-    found_associated_verb = False
+    failure_trees = set()
+    allowed_verb_tags = ('IV', 'PV', 'CV', 'ADJ.VN',
+        'NOUN.VN', 'PSEUDO_VERB', 'VERB', 'DET+ADJ.VN'
+    )
+    assoc_verb_found = False
 
     print('Starting pro-drop search... ', end='')
     for tree in treesfunc(INPUT_PATH):
         tree_count += 1
+        has_prodrop = False
         
         for pdnode in iterprodrops(tree):
-            found_associated_verb = False
-            sibling_tags = {}
+            assoc_verb_found = False
+            has_prodrop = True
             prodrop_count += 1
+            sibtags = []
 
-            # Search for primary verb tag in each sibling of a pro-drop.
-            for sibling in tree.get_siblings(pdnode.parent):
-                if sibling.is_end:
-                    update_distinct_counts(sibling_tags, sibling.tag)
-                
-                if sibling.tag.startswith('IV'):
-                    prodrop_with_verb_count += 1
-                    found_associated_verb = True
-                    update_distinct_counts(verbs, sibling.word)
-                    break
+            # Only check siblings above parent, as subject always follows
+            # verb as per the guidelines.
+            for sib in get_previous_siblings(pdnode.parent):
+                sibtags.append(sib.tag)
 
-            # If no associated verb found, store all the sibling tags of the
-            # pro-drop for the report.
-            if not found_associated_verb:
-                for tag in sibling_tags:
-                    update_distinct_counts(
-                        ignored_tags, tag, sibling_tags[tag]
-                    )
+                for tag in allowed_verb_tags:
+                    if sib.tag.startswith(tag):
+                        assoc_verb_found = True
+                        prodrop_with_verb_count += 1
+                        update_distinct_counts(verbs, sib.word)
+                        break
+
+            if not assoc_verb_found:
+                for t in sibtags:
+                    update_distinct_counts(ignored_tags, t)
+                failure_trees.add(tree)
+
+        tree_with_prodrop_count += 1 if has_prodrop else 0
   
     print('Complete.')
     _write_report(verbs, tree_count, prodrop_count,
-        prodrop_with_verb_count, ignored_tags
+        prodrop_with_verb_count, ignored_tags, failure_trees,
+        tree_with_prodrop_count, allowed_verb_tags
     )
     
-def _write_report(verbs, tree_count, prodrop_count,
-                  prodrop_with_verb_count, ignored_tags):
+def _write_report(verbs, tree_count, prodrop_count, prodrop_with_verb_count,
+                  ignored_tags, failure_trees, tree_with_prodrop_count,
+                  allowed_verb_tags):
     """ """
     path = normpath(join(OUTPUT_PATH,
         'report {0}.txt'.format(timestamp_now())
@@ -101,6 +106,12 @@ def _write_report(verbs, tree_count, prodrop_count,
     with open(path, 'w', encoding='utf8') as outfile:
         print('')
         print_and_write('Trees searched: {0}'.format(tree_count))
+        print_and_write(
+            'Trees with at least 1 pro-drop: {0}'.format(tree_with_prodrop_count)
+        )
+        print_and_write(
+            'Trees with failed sibling lookup: {0}\n'.format(len(failure_trees))
+        )
         print_and_write('Pro-drops found: {0}'.format(prodrop_count))
         print_and_write('Pro-drops with associated verb found: {0}'.format(
             prodrop_with_verb_count
@@ -109,10 +120,14 @@ def _write_report(verbs, tree_count, prodrop_count,
         print_and_write(
             'Distinct excluded sibling tags: {0}'.format(len(ignored_tags))
         )
+
+        outfile.write('\nALLOWED VERB TAG BASES\n=======================\n')
+        for tag in allowed_verb_tags:
+            outfile.write('  {0}\n'.format(tag))
         
         outfile.write('\nVERB OCCURENCES\n===============\n')
         for v in sorted(verbs, key=lambda x: verbs[x], reverse=True):
-            outfile.write('{0:>2}'.format(verbs[v]))
+            outfile.write('{0:>5}'.format(verbs[v]))
             outfile.write('  :  {0}\n'.format(v))
 
         outfile.write('\nEXCLUDED PRO-DROP SIBLING TAGS\n')
@@ -120,8 +135,15 @@ def _write_report(verbs, tree_count, prodrop_count,
         for tag in sorted(
             ignored_tags, key=lambda x: ignored_tags[x], reverse=True
         ):
-            outfile.write('{0:>4}'.format(ignored_tags[tag]))
+            outfile.write('{0:>5}'.format(ignored_tags[tag]))
             outfile.write('  :  {0}\n'.format(tag))
+
+##        outfile.write('\nTREES WITH PRO-DROPS WITH NO ASSOC. VERB FOUND:\n')
+##        outfile.write('==================================================\n')
+##        for tree in failure_trees:
+##            outfile.write(tree.treebank_notation)
+##            outfile.write('\n')
+                
 
     print('\nReport written to', path)
 
